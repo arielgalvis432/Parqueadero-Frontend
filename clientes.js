b28336aed3608902a9100dc5b7eeecce7de71f0a
Ariel <arielgalvis432@gmail.com>
2023-04-05 09:34:24 -0500

    Mostrando los datos de un cliente

--- clientes.js
+++ clientes.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
         tr.append("<td>" + e.telefono + "</td>");
         tr.append("<td>" + e.nombreParqueadero + "</td>");
         tr.append(
-          `<td><button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modalEdicion" data-bs-operacion="editar" data-bs-entidad="Parqueadero" data-bs-id="${e.id}" data-bs-nombre="${e.nombre}" data-bs-nit="${e.nit}" data-bs-direccion="${e.direccion}" data-bs-telefono="${e.telefono}">Editar</button></td>`
+          `<td><button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modalEdicion" data-bs-operacion="editar" data-bs-entidad="Cliente" data-bs-id="${e.id}" data-bs-nombre-completo="${e.nombreCompleto}" data-bs-documento="${e.documento}" data-bs-email="${e.email}" data-bs-telefono="${e.telefono}" data-bs-parquedero-id="${e.parqueaderoId}">Editar</button></td>`
         );
 
         tbyDatos.append(tr);
@@ -42,25 +42,29 @@ $(document).ready(function () {
       $("#nit").prop("disabled", true);
       const id = button.getAttribute("data-bs-id");
       const entidad = button.getAttribute("data-bs-entidad");
-      const nombre = button.getAttribute("data-bs-nombre");
+      const nombreCompleto = button.getAttribute("data-bs-nombre-completo");
 
       const titulo = exampleModal.querySelector(".modal-title");
       titulo.textContent = entidad;
 
       $("#id").val(id);
-      $("#nombre").val(nombre);
-      $("#nit").val(button.getAttribute("data-bs-nit"));
-      $("#direccion").val(button.getAttribute("data-bs-direccion"));
+      $("#nombreCompleto").val(nombreCompleto);
+      $("#documento").val(button.getAttribute("data-bs-documento"));
+      $("#email").val(button.getAttribute("data-bs-email"));
       $("#telefono").val(button.getAttribute("data-bs-telefono"));
+      
+      cargarParqueaderos(button.getAttribute("data-bs-parquedero-id"));
     } else {
       const titulo = exampleModal.querySelector(".modal-title");
-      titulo.textContent = "Nuevo Parqueadero";
+      titulo.textContent = "Nuevo Cliente";
 
-      $("#id").val("");
-      $("#nombre").val("");
-      $("#nit").val("");
-      $("#direccion").val("");
-      $("#telefono").val("");
+      $("#id").val('');
+      $("#nombreCompleto").val('');
+      $("#documento").val('');
+      $("#email").val('');
+      $("#telefono").val('');
+
+      cargarParqueaderos(0);
     }
   });
 
@@ -96,3 +100,29 @@ $(document).ready(function () {
     });
   });
 });
+
+function cargarParqueaderos(parqueaderoId) {
+  $.ajax({
+    url: "http://localhost:8080/backend/api/parqueadero",
+    type: "GET",
+    dataType: "json",
+    success: function (data) {
+      const parqueaderos = $("#parqueaderoId");
+      parqueaderos.empty();
+
+      // Recorrer el arreglo de objetos JSON que se encuentra en la propiedad 'data' del objeto JSON 'data':
+      data.forEach(function (e) {
+        const option = $("<option></option>");
+        option.val(e.id);
+        option.text(e.nombre);
+        if (e.id == parqueaderoId) {
+          option.attr("selected", "selected");
+        }
+        parqueaderos.append(option);
+      });
+    },
+    error: function (error) {
+      console.log("error", error);
+    },
+  });
+}
